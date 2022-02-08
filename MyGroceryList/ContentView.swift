42d956b1064a2655c08d7262dc1058b3a09f044f
acapodanno <alessandro.capodanno95@gmail.com>
2022-02-08 11:37:35 +0100

    Initial Commit

---
+++ MyGroceryList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyGroceryList
+//
+//  Created by Alessandro Capodanno on 08/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyGroceryList/MyGroceryListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyGroceryListApp.swift
+//  MyGroceryList
+//
+//  Created by Alessandro Capodanno on 08/02/22.
+//
+
+import SwiftUI
+
+@main
+struct MyGroceryListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
